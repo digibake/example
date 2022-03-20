066902e4e08f0ea1dcc3524792797edd2222bb70
Digibake <digibake@users.noreply.github.com>
2022-03-20 20:52:13 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# Example Javascript and Typescript
+
+Here are some javascript and typescript examples to help get started with nodejs.
+This example gets the current price of bitcoin from an api and prints it to the console. It demonstrates a lot of the core functionality such as importing and using npm packages, getting data from the web, validating data and logging (printing) it out to the console.
+
+To try out the examples:
+1. First install nodejs and visual studio code. [There are instructions here.](https://code.visualstudio.com/docs/nodejs/nodejs-tutorial)
+2. [Clone this repository.](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository)
+3. Install the packages with 'npm install'
+
+## Visual Studio Code terminal
+Visual studio code has an integrated terminal (Terminal > New Terminal) which can be used to run commands.
+
+```console
+cd javascript
+npm install
+npm start
+```
+
+```console
+cd typescript
+npm install
+npm start
+```
+
+## Resources
+- Good javascript fundamentals course https://fireship.io/courses/javascript/
+- Async Await https://www.youtube.com/watch?v=vn3tm0quoqE
+- Node Js https://nodejs.org/en/
+- Parcel Js https://parceljs.org/
---
+++ typescript/src/index.ts
@@ -0,0 +1,65 @@
+/**
+ * @file
+ * This javascript example prints the current price for bitcoin to the terminal
+ */
+import moment from "moment";
+import axios from "axios";
+import accounting from "accounting";
+import { object, string } from "yup";
+
+/**
+ * Example Main function
+ * Asynchronous wrapper that allows await syntax
+ * https://www.youtube.com/watch?v=vn3tm0quoqE
+ */
+async function main() {
+  /**
+   * Log the date to the console using the moment package
+   * https://momentjs.com/
+   */
+  console.log(moment().format("Do MMMM YYYY"));
+
+  /**
+   * Get the data from the public coinbase api
+   * https://developers.coinbase.com/api/v2#prices
+   */
+  const baseUrl = "https://api.coinbase.com/v2/prices/buy?currency=GBP";
+  const { status, data: result } = await axios.get(baseUrl);
+  const { data } = result;
+
+  /**
+   * validate that the status is 200 (okay)
+   * this show how to throw an error if something doesn't work
+   */
+  if (status !== 200) {
+    throw Error("Failed to fetch the API");
+  }
+
+  /**
+   * This is a yup schema to validate
+   * the input from the coinbase api
+   * https://github.com/jquense/yup
+   */
+  const CoinbaseSchema = object({
+    base: string().oneOf(["BTC"]).required(),
+    currency: string().oneOf(["GBP"]).required(),
+    amount: string().required(),
+  }).required();
+  /**
+   * validate that the data returned from the api fits the model
+   * this is an asynchronous call that shows how to validate
+   * and verify that the data from coinbase is good quality
+   */
+  await CoinbaseSchema.validate(data);
+
+  /**
+   * print the current price of bitcoin to the terminal
+   * using the accounting package to nicely format the number
+   * https://github.com/nashdot/accounting-js#readme
+   */
+  const { base, amount } = data;
+  console.log(base, `${accounting.formatMoney(+amount, "£")}`);
+}
+
+// run the function and catch any errors
+main().catch((e) => console.log(e));
